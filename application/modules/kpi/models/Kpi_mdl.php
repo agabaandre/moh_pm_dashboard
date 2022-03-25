b698ed765e779b329bef7c8e16eb67c9b44b38f2
Agaba Andrew <agabaandre@gmail.com>
2022-03-25 16:25:59 +0300

    dashboard 0

--- application/modules/kpi/models/Kpi_mdl.php
+++ application/modules/kpi/models/Kpi_mdl.php
@@ -85,7 +85,7 @@ class Kpi_mdl extends CI_Model {
 
 	public function kpiSummaryData(){
 
-		$query = $this->db->query("SELECT kpi.kpi_id, report_kpi_summary.kpi_id, short_name,frequency subject_areas.name FROM kpi,report_kpi_summary,subject_areas WHERE kpi.kpi_id=report_kpi_summary.kpi_id AND subject_areas.id=kpi.subject_area order by subject_areas.name ASC, short_name ASC ");
+		$query = $this->db->query("SELECT kpi.kpi_id, report_kpi_summary.kpi_id, short_name,frequency, subject_areas.name FROM kpi,report_kpi_summary,subject_areas WHERE kpi.kpi_id=report_kpi_summary.kpi_id AND subject_areas.id=kpi.subject_area order by subject_areas.name ASC, short_name ASC ");
 		return $query->result();
 	}
 
