2e2d33f4cf77e8d14c5a8018189cccaabb95be14
Agaba Andrew <agabaandre@gmail.com>
2022-05-03 16:29:42 +0300

    kpi mdl correction - bug

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,10 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'dashboard/auth/index';
+$route['default_controller'] = 'auth';
 
-$route['login']  = "dashboard/auth/login";
-$route['logout'] = "dashboard/auth/logout";
 
 
 $route['404_override'] = '';
