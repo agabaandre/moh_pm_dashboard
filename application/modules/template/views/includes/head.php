57365149ecf9f96d0a14efdc6a18edfcded127ff
AGABA ANDREW <agabaandre@gmail.com>
2022-11-15 13:47:27 +0300

    push

--- application/modules/template/views/includes/head.php
+++ application/modules/template/views/includes/head.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title><?php echo $setting->title ?> - <?php echo (!empty($title)?$title:null) ?></title>
         <!-- Favicon and touch icons -->
