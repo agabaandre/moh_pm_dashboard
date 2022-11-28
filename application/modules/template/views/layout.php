2a0f125376e2b4f5e8bf3ef23dff25018b53bc92
AGABA ANDREW <agabaandre@gmail.com>
2022-11-28 19:40:01 +0300

    menu fix

--- application/modules/template/views/layout.php
+++ application/modules/template/views/layout.php
@@ -32,22 +32,21 @@
                     <h4>
 
 
-                        <p><a href=" #"><i class="pe-7s-home"></i> Dashboard</a> -
+                        <p><a href=" #"><i class="pe-7s-home"></i> Dashboard</a>
                             <?php if (!empty($uptitle)) {
-                                echo urldecode($uptitle);
+                                echo ' - ' . urldecode($uptitle);
                             } ?>
                         </p>
 
                     </h4>
 
                     <?php //print_r(settings()); 
-                        ?>
+                    ?>
                     <div class="row" style="display:flex; float:right; margin-right:5px; margin-top:-40px;">
-                        <button href="<?php echo base_url(); ?>files/file" class="btn btn-success btn-outline"
-                            style="margin-right:5px; margin-top:-40px position: relative; <?php if ($this->uri->segment(2) == "summary") { ?> display:none;<?php } ?>"
-                            data-toggle="modal">
-                            <?php echo display("definition"); ?>
-                        </button>
+                        <a href="<?php echo base_url(); ?>files/file" class="btn btn-success btn-outline"
+                            style="margin-right:5px; margin-top:-40px position: relative; <?php if ($this->uri->segment(2) == "summary") { ?> display:none;<?php } ?>">
+                            Admin Settings
+                        </a>
 
                         <a href="<?php echo base_url(); ?>files/file" class="btn btn-success btn-outline"
                             style="margin-right:5px; margin-top:-40px position: relative; <?php if ($this->uri->segment(2) == "summary") { ?> display:none;<?php } ?>">
