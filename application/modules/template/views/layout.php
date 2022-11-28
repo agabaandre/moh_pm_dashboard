2a08f3cdf35df280fdc6daf3f636cacc985d4a5c
AGABA ANDREW <agabaandre@gmail.com>
2022-11-28 20:33:03 +0300

    menu fix

--- application/modules/template/views/layout.php
+++ application/modules/template/views/layout.php
@@ -43,11 +43,74 @@
                     <?php //print_r(settings()); 
                     ?>
                     <div class="row" style="display:flex; float:right; margin-right:5px; margin-top:-40px;">
-                        <a href="<?php echo base_url(); ?>files/file" class="btn btn-success btn-outline"
-                            style="margin-right:5px; margin-top:-40px position: relative; <?php if ($this->uri->segment(2) == "summary") { ?> display:none;<?php } ?>">
-                            Admin Settings
-                        </a>
+                        <div class="dropdown open">
+                            <a href="<?php echo base_url(); ?>files/file"
+                                class="btn btn-success btn-outline dropdown-toggle" data-toggle="dropdown"
+                                aria-expanded="true"
+                                style="margin-right:5px; margin-top:-40px position: relative; <?php if ($this->uri->segment(2) == "summary") { ?> display:none;<?php } ?>">
+                                Admin Settings
+                            </a>
+                            <ul class="dropdown-menu">
 
+                                <li>
+                                    <a href="<?php echo base_url(); ?>kpi/subject">
+                                        <span>
+                                            <?php echo "Subject Areas" ?>
+                                        </span>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="<?php echo base_url(); ?>kpi/categoryTwo">
+                                        <span>
+                                            <?php echo "Objectives /  Category Two" ?>
+                                        </span>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="<?php echo base_url(); ?>kpi/kpis">
+                                        <span>
+                                            <?php echo "Performance Indicators" ?>
+                                        </span>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="<?php echo base_url(); ?>kpi/kpiDisplay">
+                                        <span>
+                                            <?php echo "Kpi Display Control" ?>
+                                        </span>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="<?php echo base_url('dashboard/setting') ?>">
+                                        <span>
+                                            <?php echo display('application_setting') ?>
+                                        </span>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="<?php echo base_url('dashboard/language') ?>">
+                                        <span>
+                                            <?php echo display('language') ?>
+                                        </span>
+                                    </a>
+                                </li>
+
+                                <li
+                                    class="treeview <?php echo (($this->uri->segment(2) == "backup_restore") ? "active" : null) ?>">
+                                    <a href="<?php echo base_url('dashboard/backup_restore/index') ?>">
+                                        <span>
+                                            <?php echo display('backup_and_restore') ?>
+                                        </span>
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </div>
                         <a href="<?php echo base_url(); ?>files/file" class="btn btn-success btn-outline"
                             style="margin-right:5px; margin-top:-40px position: relative; <?php if ($this->uri->segment(2) == "summary") { ?> display:none;<?php } ?>">
                             Upload Data
