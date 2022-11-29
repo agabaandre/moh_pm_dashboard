9323ad2b4366a2c1556af2cb1a1d5c1d58d02963
AGABA ANDREW <agabaandre@gmail.com>
2022-11-29 10:50:20 +0300

    menu fix

--- index.php
+++ index.php
@@ -37,22 +37,22 @@
  */
 
 /*
- *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
- *---------------------------------------------------------------
- *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
- *
- * This can be set to anything, but default usage is:
- *
- *     development
- *     testing
- *     production
- *
- * NOTE: If you change these, also change the error_reporting() code below
- */
+*---------------------------------------------------------------
+* APPLICATION ENVIRONMENT
+*---------------------------------------------------------------
+*
+* You can load different configurations depending on your
+* current environment. Setting the environment also influences
+* things like logging and error reporting.
+*
+* This can be set to anything, but default usage is:
+*
+*     development
+*     testing
+*     production
+*
+* NOTE: If you change these, also change the error_reporting() code below
+/*/
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 /*
