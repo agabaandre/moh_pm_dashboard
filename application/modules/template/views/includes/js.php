0a1562c7a397acc22306a6f1cccf124436e944de
AGABA ANDREW <agabaandre@gmail.com>
2023-01-29 16:53:14 +0300

    dashboard

--- application/modules/template/views/includes/js.php
+++ application/modules/template/views/includes/js.php
@@ -1,42 +1,42 @@
-         <script src="<?php echo base_url()?>assets/plugins/jQuery/jquery-1.12.4.min.js" type="text/javascript"></script>
-        <!-- jquery-ui --> 
-        <script src="<?php echo base_url() ?>assets/plugins/jquery-ui-1.12.1/jquery-ui.min.js" type="text/javascript"></script>
-        <!-- Bootstrap -->
-        <script src="<?php echo base_url() ?>assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <!-- lobipanel -->
-        <script src="<?php echo base_url() ?>assets/plugins/lobipanel/lobipanel.min.js" type="text/javascript"></script>
-        <!-- Pace js -->
-        <script src="<?php echo base_url() ?>assets/plugins/pace/pace.min.js" type="text/javascript"></script>
-        <!-- SlimScroll -->
-            <script src="<?php echo base_url('assets/js/jquery.slimscroll.min.js') ?>" type="text/javascript"></script>  
-        <!-- bootstrap timepicker -->
-        <script src="<?php echo base_url() ?>assets/js/jquery-ui-sliderAccess.js" type="text/javascript"></script> 
-        <script src="<?php echo base_url() ?>assets/js/jquery-ui-timepicker-addon.min.js" type="text/javascript"></script> 
-        <!-- select2 js -->
-        <script src="<?php echo base_url() ?>assets/js/select2.min.js" type="text/javascript"></script>
-        <script src="<?php echo base_url('assets/js/sparkline.min.js') ?>" type="text/javascript"></script> 
-        <script src="<?php echo base_url('assets/js/jquery.counterup.min.js') ?>" type="text/javascript"></script>
-        <!-- ChartJs JavaScript -->
-        <script src="<?php echo base_url('assets/js/Chart.min.js?v=2.5') ?>" type="text/javascript"></script>
-        <!-- DataTables JavaScript -->
+<script src="<?php echo base_url() ?>assets/plugins/jQuery/jquery-1.12.4.min.js" type="text/javascript"></script>
+<!-- jquery-ui -->
+<script src="<?php echo base_url() ?>assets/plugins/jquery-ui-1.12.1/jquery-ui.min.js" type="text/javascript"></script>
+<!-- Bootstrap -->
+<script src="<?php echo base_url() ?>assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<!-- lobipanel -->
+<script src="<?php echo base_url() ?>assets/plugins/lobipanel/lobipanel.min.js" type="text/javascript"></script>
+<!-- Pace js -->
+<script src="<?php echo base_url() ?>assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+<!-- SlimScroll -->
+<script src="<?php echo base_url('assets/js/jquery.slimscroll.min.js') ?>" type="text/javascript"></script>
+<!-- bootstrap timepicker -->
+<script src="<?php echo base_url() ?>assets/js/jquery-ui-sliderAccess.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/js/jquery-ui-timepicker-addon.min.js" type="text/javascript"></script>
+<!-- select2 js -->
+<script src="<?php echo base_url() ?>assets/js/select2.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/js/sparkline.min.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/js/jquery.counterup.min.js') ?>" type="text/javascript"></script>
+<!-- ChartJs JavaScript -->
+<script src="<?php echo base_url('assets/js/Chart.min.js?v=2.5') ?>" type="text/javascript"></script>
+<!-- DataTables JavaScript -->
 
-        <script type="text/javascript" src="<?php echo base_url('assets/css/DataTables/datatables.min.js'); ?>"></script>
-        <!-- Table Head Fixer -->
-        <script src="<?php echo base_url() ?>assets/js/tableHeadFixer.js" type="text/javascript"></script> 
-        <!-- Admin Script -->
-        <script src="<?php echo base_url('assets/js/frame.js') ?>" type="text/javascript"></script> 
-        <script src="<?php echo base_url('assets/js/bootstrap-toggle.min.js') ?>" type="text/javascript"></script> 
-        <script src="<?php echo base_url() ?>assets/js/sweetalert/sweetalert.min.js" type="text/javascript"></script>
-        <!-- Custom Theme JavaScript -->
-        <script src="<?php echo base_url() ?>assets/js/custom.js" type="text/javascript"></script>
-        <script src="<?php echo base_url('assets/js/datetimepicker.js') ?>" type="text/javascript"></script>
-    
-      
+<script type="text/javascript" src="<?php echo base_url('assets/css/DataTables/datatables.min.js'); ?>"></script>
+<!-- Table Head Fixer -->
+<script src="<?php echo base_url() ?>assets/js/tableHeadFixer.js" type="text/javascript"></script>
+<!-- Admin Script -->
+<script src="<?php echo base_url('assets/js/frame.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/js/bootstrap-toggle.min.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/js/sweetalert/sweetalert.min.js" type="text/javascript"></script>
+<!-- Custom Theme JavaScript -->
+<script src="<?php echo base_url() ?>assets/js/custom.js" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/js/datetimepicker.js') ?>" type="text/javascript"></script>
+
+
 <!-- End Core Plugins -->
- <!-- Start Page Lavel Plugins
+<!-- Start Page Lavel Plugins
         =====================================================================-->
-    
-        <!-- End Page Lavel Plugins
+
+<!-- End Page Lavel Plugins
         =====================================================================-->
 
 <!-- Dashboard js -->
@@ -44,43 +44,44 @@
 
 <!-- End Theme label Script-->
 <script type="text/javascript">
-$(document).ready(function() {
-    $('.js-example-basic-single').select2();
-});
+    $(document).ready(function () {
+        $('.js-example-basic-single').select2();
+    });
 
 </script>
 <script type="text/javascript">
-    $(document).ready(function() {
-     $('.js-example-basic-multiple').select2();
+    $(document).ready(function () {
+        $('.js-example-basic-multiple').select2();
 
     });
-$(document).ready(function() {
-    $("#fullscreen-btn").click(function() {
-        var el = document.getElementById("fill-screen");
-        if (el.requestFullscreen) {
-            el.requestFullscreen();
-        } else if (el.mozRequestFullScreen) {
-            el.mozRequestFullScreen();
-        } else if (el.webkitRequestFullscreen) {
-            el.webkitRequestFullscreen();
-        } else if (el.msRequestFullscreen) {
-            el.msRequestFullscreen();
-        }
+    $(document).ready(function () {
+        $("#fullscreen-btn").click(function () {
+            var el = document.getElementById("fill-screen");
+            if (el.requestFullscreen) {
+                el.requestFullscreen();
+            } else if (el.mozRequestFullScreen) {
+                el.mozRequestFullScreen();
+            } else if (el.webkitRequestFullscreen) {
+                el.webkitRequestFullscreen();
+            } else if (el.msRequestFullscreen) {
+                el.msRequestFullscreen();
+            }
+        });
     });
-});
 </script>
+
 
 
 
 <!-- Include module Script -->
 <?php
-    $path = 'application/modules/';
-    $map  = directory_map($path);
-    if (is_array($map) && sizeof($map) > 0)
+$path = 'application/modules/';
+$map = directory_map($path);
+if (is_array($map) && sizeof($map) > 0)
     foreach ($map as $key => $value) {
-        $js   = str_replace("\\", '/', $path.$key.'assets/js/script.js'); 
+        $js = str_replace("\\", '/', $path . $key . 'assets/js/script.js');
         if (file_exists($js)) {
-            echo "<script src=".base_url($js)." type=\"text/javascript\"></script>";
-        }   
-    }   
+            echo "<script src=" . base_url($js) . " type=\"text/javascript\"></script>";
+        }
+    }
 ?>
