58c6db848139bcd6f966eda09815709972a0ce2a
Agaba Andrew <agabaandre@gmail.com>
2024-07-02 00:05:25 +0300

    fy

--- application/modules/dashboard/views/home/partials/filters.php
+++ application/modules/dashboard/views/home/partials/filters.php
@@ -38,37 +38,7 @@
         </select>
     </div>
     <div class="form-group col-md-3 col-sm-12">
-        <label for="financial_year">Financial Year:(*)</label>
-
-    <select class="form-control selectize" name="financial_year" required>
-    <option value="">Select Financial Year</option>
-    <?php
-            
-                $current_date = date('Y-m-d');
-                $current_year = date('Y', strtotime($current_date));
-                $next_year = $current_year + 1;
-                if (date('m-d', strtotime($current_date)) < '06-30') {
-        
-                    $current_year -= 1;
-                    $next_year -= 1;
-                }
-                $current_financial_year = $current_year . '-' . $next_year;
-                $startdate = "2022"; // Start of available financial years
-                $enddate = intval(date('Y') + 1); // End of available financial years
-                $years = range($startdate, $enddate);
-
-                foreach ($years as $year) {
-                    $financial_year = $year . '-' . ($year + 1);
-                    ?>
-                    <option value="<?php echo $financial_year; ?>" <?php if (($current_financial_year === $financial_year)|| ($financial_year===$this->input->get('financial_year)'))) {
-                           echo "selected";
-                       } ?>>
-                        <?php echo $financial_year; ?>
-                    </option>
-                <?php } 
-                
-                ?>
-     </select>
+       
 
  
   
